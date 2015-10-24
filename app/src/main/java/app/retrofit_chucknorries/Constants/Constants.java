c932cdd2a638983d148b30cfd8386e4b48c29c18
sevakkalpesh <sevakkalpesh@gmail.com>
2015-10-24 17:04:38 -0400

    Jenkins Demo error 1.1.

--- app/src/main/java/app/retrofit_chucknorries/Constants/Constants.java
+++ app/src/main/java/app/retrofit_chucknorries/Constants/Constants.java
@@ -4,7 +4,7 @@ package app.retrofit_chucknorries.Constants;
  * Created by kalpesh on 13/10/2015.
  */
 public class Constants {
-//BASE URL LINK:JENKINS DEMO
-    public static final String BASE_URL="http://api.icndb.com/jokes;
+//BASE URL LINK:JENKINS DEMO1.1
+    public static final String BASE_URL="http://api.icndb.com/jokes ;
 
 }
