fbec13b7e1ba43c8aa0a462a2f8e013ea45057ea
sevakkalpesh <sevakkalpesh@gmail.com>
2015-10-25 22:53:55 -0400

    Jenkins

--- app/src/main/java/app/retrofit_chucknorries/Constants/Constants.java
+++ app/src/main/java/app/retrofit_chucknorries/Constants/Constants.java
@@ -4,7 +4,7 @@ package app.retrofit_chucknorries.Constants;
  * Created by kalpesh on 13/10/2015.
  */
 public class Constants {
-//BASE URL LINK:JENKINS DEMO
+//BASE URL LINK:JENKINS DEMO2.0
     public static final String BASE_URL="http://api.icndb.com/jokes";
 
 }
